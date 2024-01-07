222a753a426f8aec6221c2b1726f7ee18a19dc27
Baqar <baqarmehdi777@gmail.com>
2024-01-07 20:34:04 +0500

    Added a frontend of an  Expense tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderCom />
+  <div class="container">
+    <BalanceCom />
+    <IncomeExpense />
+    <!-- binding transactions function to transactions -->
+    <TransactionList v-bind:transactions="transactions" />
+    <AddTransaction />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {ref} from 'vue'
+import HeaderCom from "./components/HeaderCom.vue";
+import BalanceCom from "./components/BalanceCom.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const transactions = ref( [
+  { id: 1, text: "Flower", amount: -19.99 },
+  { id: 2, text: "Salary", amount: 299.97 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+]);
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HeaderCom,
+//     BalanceCom,
+//     IncomeExpense,
+//     TransactionList,
+//     AddTransaction
+//   }
+// }
 </script>
 
 <style>
-#app {
+/* @import url(./assets/style.css); */
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,61 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <!-- Using for loop to show and class binding to show  -+ with ternary operator -->
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}<span>$ {{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+// const props = defineProps
+defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    },
+});
+
+</script>
+
+
+<!-- scripting with composition api -->
+
+
+<!-- export default {
+  for option API
+    data() {
+      return {
+        transactions: [
+          { id: 1, text: "Flower", amount: -19.99 },
+          { id: 2, text: "Salary", amount: 299.97 },
+          { id: 3, text: "Book", amount: -10 },
+          { id: 4, text: "Camera", amount: 150 },
+        ],
+      };
+    },
+  for composition API  old way
+  setup() {
+    Making a variable first
+    const transactions = [
+      { id: 1, text: "Flower", amount: -19.99 },
+      { id: 2, text: "Salary", amount: 299.97 },
+      { id: 3, text: "Book", amount: -10 },
+      { id: 4, text: "Camera", amount: 150 },
+    ];
+    and then returnig
+    return {
+        transactions,
+    }
+  },
+}; -->
+<!-- <script setup></script>
+</script> -->
