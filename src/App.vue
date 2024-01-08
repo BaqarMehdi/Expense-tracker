c27bcb3b4e9d967f9ba849450532cb2b1a1b7a9c
Baqar <baqarmehdi777@gmail.com>
2024-01-08 14:15:13 +0500

    Added frontend and some features

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <HeaderCom />
   <div class="container">
-    <BalanceCom />
-    <IncomeExpense />
+    <BalanceCom v-bind:total="total" />
+    <IncomeExpense v-bind:income="+income" v-bind:expenses="-expenses" />
     <!-- binding transactions function to transactions -->
     <TransactionList v-bind:transactions="transactions" />
     <AddTransaction />
@@ -10,19 +10,76 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref, computed } from "vue";
 import HeaderCom from "./components/HeaderCom.vue";
 import BalanceCom from "./components/BalanceCom.vue";
 import IncomeExpense from "./components/IncomeExpense.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-const transactions = ref( [
+const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
   { id: 2, text: "Salary", amount: 299.97 },
   { id: 3, text: "Book", amount: -10 },
   { id: 4, text: "Camera", amount: 150 },
 ]);
+
+// console.log(transactions)
+
+// const total = computed(() => {
+//   return transactions.value.reduce((acc, transaction) => {
+//     return acc + transaction.amount;
+//   }, 0);
+// });
+
+// get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+// get income
+
+// const income = computed (() => {
+//   return transactions.value
+//   .filter((transaction) =>
+//     transaction.amount > 0
+//   ).
+//   reduce((acc, transaction) =>{
+//     return acc + transaction.amount
+//   }, 0).toFixed(2);
+// });
+
+// get expense
+
+// const expense = computed (() => {
+//   return transactions.value.filter((transaction) =>
+//     transaction.amount < 0
+//   ).
+//   reduce((acc, transaction) =>{
+//     return acc + transaction.amount
+//   }, 0).toFixed(2);
+// });
+
+//
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
 
 // export default {
 //   name: 'App',
